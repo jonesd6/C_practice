d964ed29356d1790c3867c5bce68cee7bc05031e
Don Jones <jonesd6000@gmail.com>
2018-02-01 21:36:50 -0800

    Linked list practice in C.

---
+++ linked_list.c
@@ -0,0 +1,87 @@
+// Simple C program to introduce
+// a linked list.
+
+#include <stdlib.h>
+#include <stdio.h>
+
+struct node {
+    int data;
+    struct node * next;
+};
+
+int main() {
+
+    struct node* head = NULL;
+    struct node* second = NULL;
+    struct node* third = NULL;
+
+    // allocate 3 nodes in the heap
+    head = (struct node*)malloc(sizeof(struct node));
+    second = (struct node*)malloc(sizeof(struct node));
+    third = (struct node*)malloc(sizeof(struct node));
+
+    /* Three blocks have been allocated dynamically.
+       We have pointers to these three blocks as first,
+       second and third
+        head		second		third
+         |		|		|
+	 |		|		|
+      *-------*     *-------*       *-------*
+      | # | # |     | # | # |       | # | # |
+      *-------*     *-------*       *-------*
+
+     # reresents any random value.
+     Data s random because we haven't assigned anythign yet  */
+
+    head->data = 1; //assign data in first node
+    head->next = second; // Link first node with the second node
+
+    /* Data has been assigned to data part of the first block
+       (block pointed by head). And next pointer of first block
+       points to second. So they both are linked.
+
+        head		second		third
+         |		|		|
+	 |		|		|
+      *-------*     *-------*       *-------*
+      | 1 | o ----> | # | # |       | # | # |
+      *-------*     *-------*       *-------*
+    */
+
+    second->data = 2; // assign data to second node
+    second->next = third; // Link second node with the third.
+
+    /*
+
+        head		second		third
+         |		|		|
+	 |		|		|
+      *-------*     *-------*       *-------*
+      | 1 | o ----> | 2 | o ---->   | # | # |
+      *-------*     *-------*       *-------*
+    */
+
+    third->data = 3; // assign data to third node.
+    third->next = NULL;
+
+    /*
+
+        head		second		third
+         |		|		|
+	 |		|		|
+      *-------*     *-------*       *---------*
+      | 1 | o ----> | 2 | o ---->   | 3 | NULL|
+      *-------*     *-------*       *---------*
+    
+    Note that only head is sufficent to represent the whole
+    list. We can traverse the complete list by following next
+    pointers.
+    
+    The next pointer of the third block is made NULL to
+    indicate that the linked list is terminated here.
+    */
+
+      
+    return 0;
+}
+
